d8562d0d2b2d319ade0a2065f8741bfcbabd7e51
Zuhair <zuhair_m_h@yahoo.com>
2024-03-19 12:41:00 -0500

    Update converter.js

--- converter.js
+++ converter.js
@@ -40,3 +40,28 @@ form.addEventListener('input', () => {
   const outputTemp = convertTemp(inputTemp, fromUnit, toUnit);
   outputField.value = (Math.round(outputTemp * 100) / 100) + ' ' + toUnit.toUpperCase();
 });
+
+
+const debounce = (func, wait) => {
+  let timeout;
+  return function executedFunction(...args) {
+    const later = () => {
+      clearTimeout(timeout);
+      func(...args);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
+};
+
+if ('windowControlsOverlay' in navigator) {
+  navigator.windowControlsOverlay.addEventListener('geometrychange', debounce(e => {
+    // Detect if the Window Controls Overlay is visible.
+    const isOverlayVisible = navigator.windowControlsOverlay.visible;
+
+    // Get the size and position of the title bar area.
+    const titleBarRect = e.titlebarAreaRect;
+
+    console.log(`The overlay is ${isOverlayVisible ? 'visible' : 'hidden'}, the title bar width is ${titleBarRect.width}px`);
+  }, 200));
+}
