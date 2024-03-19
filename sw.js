3dee61fd716d2b9c01abfd6c4c9ddb7986c58e4a
Zuhair <zuhair_m_h@yahoo.com>
2024-03-19 11:35:28 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,9 +5,9 @@ self.addEventListener('install', event => {
   event.waitUntil((async () => {
     const cache = await caches.open(CACHE_NAME);
     cache.addAll([
-      '/',
-      '/converter.js',
-      '/converter.css'
+      '/pwapp/',
+      '/pwapp/converter.js',
+      '/pwapp/converter.css'
     ]);
   })());
 });
